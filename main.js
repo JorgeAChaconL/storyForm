dc044bb2ba109807c6896e34a6e9edcdc48a26c7
JorgeAChacon <62924612+JorgeAChaconL@users.noreply.github.com>
2025-01-23 20:58:33 -0600

    Grab and populate the title with the information provided, BRUTUS MODE

--- main.js
+++ main.js
@@ -34,4 +34,11 @@ const story = `<p>A <span class="word" title="id: animal-1">${firstAnimal}</span
 
 <p>The <span class="word" title="id: animal-2">${secondAnimal}</span> meanwhile kept going slowly but steadily, and, after a time, passed the place where the <span class="word" title="animal-1">${firstAnimal}</span> was <span class="word" title="id: verb-1">${verb1}</span>. The <span class="word" title="id: animal-1">${firstAnimal}</span> was so caught up in <span class="word" title="id: verb-1">${verb1}</span>; and when at last he did stop, the <span class="word" title="id: animal-2">${secondAnimal}</span> was near the goal. The <span class="word" title="id: animal-1">${firstAnimal}</span> now ran his swiftest, but he could not overtake the <span class="word" title="id: animal-2">${secondAnimal}</span> in time.</p>`;
 
+//Get elements for the titles.
+const title = document.getElementById('title');
+
+//Insert this elements on the title 
+
+title.innerHTML = `The <span class="word" title="id: animal-1">${firstAnimal}</span> And The  <span class="word" title="id: animal-2">${secondAnimal}</span>`;
+
 
